3d1d276265e2db15ade752b8ae6d49cd419c727f
MOABdev <abmohammed659@gmail.com>
2024-09-04 14:37:42 +0000

    Added validation to the signIn and signUp screens using hook-form and yup

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,56 @@
 import { FcGoogle } from "react-icons/fc";
 import { RiLoginBoxLine } from "react-icons/ri";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const schema = yup.object({
+  email: yup.string().email().required("Email is required"),
+  password: yup.string().min(5).max(20).required("Password is required"),
+});
 
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const handleFormSubmission = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <div className="sign-in">
-            <h2>Sign In <RiLoginBoxLine style={{marginLeft: "5px"}}/></h2>
-      <form>
-        <input type="email" placeholder="Email Here..." />
-        <input type="password" placeholder="Password Here..." />
+      <h2>
+        Sign In <RiLoginBoxLine style={{ marginLeft: "5px" }} />
+      </h2>
+      <form onSubmit={handleSubmit(handleFormSubmission)}>
+        <input
+          type="email"
+          placeholder="Email Here..."
+          {...register("email")}
+        />
+        {errors && <p>{errors.email?.message}</p>}
+        <input
+          type="password"
+          placeholder="Password Here..."
+          {...register("password")}
+        />
+        {errors && <p>{errors.password?.message}</p>}
         <button>Sign In</button>
       </form>
-      <button className="signInWithGoogleBtn">Sign In With Google <FcGoogle style={{marginLeft: "5px"}}/></button>
+      <button className="signInWithGoogleBtn">
+        Sign In With Google <FcGoogle style={{ marginLeft: "5px" }} />
+      </button>
 
       <Link to="/sign-up">Don't have an account? Sign Up</Link>
     </div>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,12 +1,40 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+type FormData = {
+  email: string
+  password: string
+}
+
+const schema = yup.object({
+  email: yup.string().email().required("Email is required"),
+  password: yup.string().min(5).max(20).required("Password is required")
+})
 
 const SignUp = () => {
+
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  });
+
+ 
+
+  const handleFormSubmission = (data: FormData) => {
+console.log(data);
+  }
+
+
+
   return (
     <div className="sign-up">
       <h2>Sign Up</h2>
-      <form>
-        <input type="email" placeholder="Email Here..." />
-        <input type="password" placeholder="Password Here..." />
+      <form onSubmit={handleSubmit(handleFormSubmission)}>
+        <input type="email" placeholder="Email Here..." {...register("email")}/>
+        {errors && <p style={{color: "red"}}>{errors.email?.message}</p>}
+        <input type="password" placeholder="Password Here..." {...register("password")}/>
+        {errors && <p style={{color: "red"}}>{errors.password?.message}</p>}
         <button>Sign Up</button>
       </form>
       <Link to="/sign-in"><h4>Sign In</h4></Link>
