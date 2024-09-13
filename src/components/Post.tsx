7431d909fc92b1ae94e21d9d1db5d71c06ecf0d3
MOABdev <abmohammed659@gmail.com>
2024-09-13 16:09:21 +0000

    Delete function implemented (firebase)

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,27 +1,76 @@
-import { CgProfile } from "react-icons/cg";
-import { Posts } from "../screens/Home";
+// import { doc, deleteDoc } from "firebase/firestore";
+// import { db } from "../config/firebase";
+// import { CgProfile } from "react-icons/cg";
+// import { SlDislike } from "react-icons/sl";
+// import { SlLike } from "react-icons/sl";
+// import { RiDeleteBin7Line } from "react-icons/ri";
+// import { Posts } from "../screens/Home";
+// import { useState, useEffect } from "react";
 
-interface Props {
-    post: Posts;
-}
+// interface Props {
+//   post: Posts;
+// }
 
-const Post = (props: Props) => {
-    const { post } = props;
+// const Post = (props: Props) => {
+//   const { post } = props;
+//   const [postInArr, setPostInArr] = useState([post])
 
+// //   useEffect(() => {
+// //     const q = query(collection(db, "posts"));
+// //     const unsubscribe = onSnapshot(q, (snapshot) => {
+// //       const updatedPosts = snapshot.docs.map((doc) => ({
+// //         id: doc.id,
+// //         ...doc.data(),
+// //       })) as Posts[];
 
+// //       // Update the state with the new posts data, including deletions
+// //       setPostInArr(updatedPosts);
+// //     });
 
+// //     // Cleanup the listener when the component unmounts
+// //     return () => unsubscribe();
+// //   }, []);
 
-  return (
-    <div className="post">
-      <div className="header">
-        { (post.photoLink == null)? <CgProfile style={{width: "20", height: "20", margin: "0"}}/> : <img src={post.photoLink} width="20" height="20"/>}
-        <p>{ post.useremail }</p>
-      </div>
-      <div className="message">
-        <p>{ post.writeup }</p>
-      </div>
-    </div>
-  );
-};
+//   const deletePost = async () => {
+//     try {
+//       await deleteDoc(doc(db, "posts", post.id));
+//       setPostInArr(postInArr.filter(post => !post))
+//       console.log("Post deleted");
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
 
-export default Post;
+//   const likePost = async () => {};
+
+//   const disLikePost = async () => {};
+
+//   return (
+//     <div className="post">
+//       <div className="header">
+//         {post.photoLink == null ? (
+//           <CgProfile style={{ width: "20", height: "20", margin: "0" }} />
+//         ) : (
+//           <img src={post.photoLink} width="20" height="20" />
+//         )}
+//         <p>{post.useremail}</p>
+//       </div>
+//       <div className="message">
+//         <p>{post.writeup}</p>
+//       </div>
+//       <div className="footer">
+//         <button onClick={likePost}>
+//           <SlLike />
+//         </button>
+//         <button onClick={disLikePost}>
+//           <SlDislike />
+//         </button>
+//         <button onClick={deletePost}>
+//           <RiDeleteBin7Line />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Post;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { auth, db } from "../config/firebase"
 import { useAuthState } from "react-firebase-hooks/auth"
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, deleteDoc, doc, query, where } from "firebase/firestore"
 import { useEffect, useState } from "react"
-import Post from "../components/Post";
+import { CgProfile } from "react-icons/cg";
+import { SlDislike } from "react-icons/sl";
+import { SlLike } from "react-icons/sl";
+import { RiDeleteBin7Line } from "react-icons/ri";
+// import Post from "../components/Post";
 
 
 export interface Posts {
@@ -13,6 +17,7 @@ export interface Posts {
     userId: string;
     id: string;
   }
+
 
 const Home = () => {
 
@@ -27,16 +32,58 @@ const Home = () => {
         data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Posts[]
       );
     };
+
+    const deletePost = async (id: string, userId: string) => {
+      try {
+        const queryUserPosts = query(postsRef, where("userId", "==", userId))
+        const userPosts = await getDocs(queryUserPosts)
+        await deleteDoc(doc(db, "posts", id));
+        setPostLists((prev) => prev && prev.filter((post) => post.id !== userPosts.docs[0].id ))
+      
+        console.log("Post deleted");
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    const likePost = async () => {};
+  
+    const disLikePost = async () => {};
   
     useEffect(() => {
       getPosts();
     }, []);
 
     return ( 
+
         <div className="home">
             { user? <><h1>Welcome {user.email}</h1>
             { postLists?.map((post) =>
-                 <Post post={post}/>
+                //  <Post post={post} />
+                <div className="post">
+                <div className="header">
+                  {post.photoLink == null ? (
+                    <CgProfile style={{ width: "20", height: "20", margin: "0" }} />
+                  ) : (
+                    <img src={post.photoLink} width="20" height="20" />
+                  )}
+                  <p>{post.useremail}</p>
+                </div>
+                <div className="message">
+                  <p>{post.writeup}</p>
+                </div>
+                <div className="footer">
+                  <button onClick={likePost}>
+                    <SlLike />
+                  </button>
+                  <button onClick={disLikePost}>
+                    <SlDislike />
+                  </button>
+                  <button onClick={() => deletePost(post.id, post.userId)}>
+                    <RiDeleteBin7Line />
+                  </button>
+                </div>
+              </div>
             )}
             </>
 
